35ea548f385a07007a1cf99b6d2a2785e7b3e617
Andriy Herasymenko <andriyherasymenko@andriys-mbp.home>
2017-02-07 14:38:45 +0400

    Initial Commit

---
+++ AirPort/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AirPort
+//
+//  Created by Andriy Herasymenko on 2/7/17.
+//  Copyright © 2017 Andriy Herasymenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
